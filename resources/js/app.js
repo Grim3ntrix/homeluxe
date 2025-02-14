17b73097437d911eec7d8dcec14fc3cbdd42c8d2
unknown <richardsamberi101@gmail.com>
2025-02-14 20:49:58 +0800

    chore: set-up Inertia.js with Ziggy for routing support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { ZiggyVue } from 'ziggy';
+import { Ziggy } from './ziggy';
 import './bootstrap';
+
+createInertiaApp({
+    resolve: (name) => import(`./Pages/${name}.vue`), // Adjust path if your Vue pages are in a different folder
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+});
