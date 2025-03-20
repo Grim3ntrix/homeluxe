0ab48b7692b13aee7af72a7cd7ea1b8885cc4d62
Richard Samberi <richardsamberi101@gmail.com>
2025-03-20 23:34:56 +0800

    feat(auth): install Laravel Breeze with Inertia.js for authentication

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -8,6 +8,6 @@ use Inertia\Inertia;
 class GuestController extends Controller
 {
     public function index() {
-        return Inertia::render('Guests/index');
+        // return Inertia::render('Guests/Home');
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,27 @@
+import '../css/app.css';
 import './bootstrap';
-import { createApp, h } from 'vue';
+
 import { createInertiaApp } from '@inertiajs/vue3';
-import { ZiggyVue } from './../../vendor/tightenco/ziggy';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-// Preload all .vue files inside Pages/** (including subdirectories)
-const pages = import.meta.glob('./Pages/**/*.vue');
-
-async function resolvePageComponent(name) {
-    const path = `./Pages/${name.replace('.', '/')}.vue`;
-
-    if (!pages[path]) {
-        console.error(`Error loading page component: ${name} at ${path}`);
-        throw new Error(`Page component ${name} not found`);
-    }
-
-    return pages[path]().then(module => module.default);
-}
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    resolve: resolvePageComponent,
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
     setup({ el, App, props, plugin }) {
-        const app = createApp({ render: () => h(App, props) });
-
-        app.use(plugin)
-           .use(ZiggyVue)
-           .mount(el);
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
     },
 });
